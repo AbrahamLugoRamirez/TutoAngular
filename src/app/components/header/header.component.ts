ff81032b40d7ba1dc2310568a8276123150b7e2e
Abraham Lugo Ramirez <labrahamdedios@gmail.com>
2021-01-27 14:44:55 -0500

    Sumar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log("Componente Header creandose!")
+  }
 
   ngOnInit(): void {
+    console.log("Componente Header Creado!")
+  }
+
+  nombre = "Abraham Lugo Ramirez"
+
+  arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  sumar(){
+    this.arr.push(this.arr.length)
   }
 
 }

97778c93d70f650784bfe989641d85cd2a14cb61
Abraham Lugo Ramirez <labrahamdedios@gmail.com>
2021-01-27 15:57:33 -0500

    User-Condiciones y plantilla

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -14,13 +14,9 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
     console.log("Componente Header Creado!")
   }
-
   nombre = "Abraham Lugo Ramirez"
-
   arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
   sumar(){
     this.arr.push(this.arr.length)
   }
-
 }

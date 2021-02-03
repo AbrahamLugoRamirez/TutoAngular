68e948fa99b9fb569c73cf94b0d31298a00b4749
Abraham Lugo Ramirez <labrahamdedios@gmail.com>
2021-02-03 11:08:23 -0500

    Parte 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { UserComponent } from './components/user/user.component';
+import { Parte4Component } from './components/parte4/parte4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    UserComponent
+    UserComponent,
+    Parte4Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/parte4/parte4.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'parte4',
+  templateUrl: './parte4.component.html',
+  styleUrls: ['./parte4.component.sass']
+})
+export class Parte4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  desactivado = false;
+
+}

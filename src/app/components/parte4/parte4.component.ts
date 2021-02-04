677fd97ff8ae4964e1eb7bbd59e3d1760d7bc11f
Abraham Lugo Ramirez <labrahamdedios@gmail.com>
2021-02-04 17:57:47 -0500

    Boopstrap

--- src/app/components/parte4/parte4.component.ts
+++ src/app/components/parte4/parte4.component.ts
@@ -6,12 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parte4.component.sass']
 })
 export class Parte4Component implements OnInit {
-
   constructor() { }
-
   ngOnInit(): void {
   }
   desactivado = false;
-  nombre:string = ""
-
+  nombre: string = ""
 }
